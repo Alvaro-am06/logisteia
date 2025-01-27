7f04d400c616798b61fd74664c20ffcaae2308bf
alvaro andrades <aandradesm01@educarex.es>
2025-01-27 10:15:09 +0100

    Unir vistas hechas

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { PanelAdmin } from './panel-admin/panel-admin';
+import { Plantilla } from './plantilla/plantilla';
+import { Usuarios } from './usuarios/usuarios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: App },
+  { path: 'panel-admin', component: PanelAdmin },
+  { path: 'plantilla', component: Plantilla },
+  { path: 'usuarios', component: Usuarios },
+];
