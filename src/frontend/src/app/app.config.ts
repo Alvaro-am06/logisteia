45fcd51fc350a1b5935d449f23de12f312f339b5
alvaro andrades <aandradesm01@educarex.es>
2025-01-27 10:15:09 +0100

    Corregir fallos angular

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -2,19 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-<<<<<<< HEAD
-=======
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
->>>>>>> sprint1-alvaro
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-<<<<<<< HEAD
-    provideRouter(routes)
-=======
     provideRouter(routes), provideClientHydration(withEventReplay())
->>>>>>> sprint1-alvaro
   ]
 };
