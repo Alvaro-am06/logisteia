1f61257b2eb84434343e32e1ad4904f1641222c6
Alvaro-am06 <alvaroandrades1506@gmail.com>
2026-01-25 08:38:07 +0100

    Integrar Tailwind

---
+++ src/frontend/src/app/login/login.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.html',
+})
+export class Login {
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  email = '';
+  password = '';
+  message = '';
+
+  onSubmit() {
+    const startTime = Date.now();
+    console.log('Iniciando petición de login...');
+
+    this.http.post('http://localhost/logisteia/src/www/api/login.php', { email: this.email, password: this.password })
+      .subscribe({
+        next: (response: any) => {
+          const endTime = Date.now();
+          console.log(`Petición completada en ${endTime - startTime} ms`);
+
+          if (response.success) {
+            this.message = 'Login exitoso';
+            this.router.navigate(['/panel-admin']);
+          } else {
+            this.message = 'Error: ' + response.error;
+          }
+        },
+        error: (error) => {
+          const endTime = Date.now();
+          console.log(`Error en ${endTime - startTime} ms:`, error);
+          this.message = 'Error de conexión: ' + error.message;
+        }
+      });
+  }
+}
