9b3457f4b5670006d4153d739b58d4b79401f47a
Alvaro-am06 <alvaroandrades1506@gmail.com>
2026-01-25 08:48:04 +0100

    Arreglar fallos de Funcionalidad

--- src/www/guardar_cliente.php
+++ src/www/guardar_cliente.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../controladores/ControladorCliente.php';
+require_once 'controladores/ControladorCliente.php';
 
 $controller = new ControladorDeCliente();
 $controller->guardar();
