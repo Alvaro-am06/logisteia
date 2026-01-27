7773102448c076226b8748f595d0c5ffdb23acb6
alvaro andrades <aandradesm01@educarex.es>
2026-01-27 06:52:07 +0100

    Corregir fallos angular

--- src/frontend/src/app/app.spec.ts
+++ src/frontend/src/app/app.spec.ts
@@ -18,10 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
-=======
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-app');
->>>>>>> sprint1-alvaro
   });
 });
